f8f5d454093b331eac986565b6ce295d5d36ce60
SorenHersomNielsen <hersom@ellekrattet.dk>
2020-11-25 12:58:27 +0100

    MVVM
    Vi har lavet vores model af patient, og gået i gang med at lave view og viewmodel

---
+++ FysioterapiMVVM/Patient.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FysioterapiMVVM
+{
+    public class Patient
+    {
+        // instancefield (her gemmer vi vores patients info)
+        private string navn;
+        private int cprnr;
+        private int tlfnr;
+        private string sygdom;
+        private string adresse;
+        private string email;
+
+        // Alt dette her er properties kan ses på kendetegnet (get; set;)
+        public string Navn { get { return navn; } set { navn = value; } }
+        public int Cprnr { get { return cprnr; } set { cprnr = value; } }
+        public int Tlfnr { get { return tlfnr; } set { tlfnr = value; } }
+        public string Sygdom { get { return sygdom; } set { sygdom = value; } }
+        public string Adresse { get { return adresse; } set { adresse = value; } }
+        public string Email { get { return email; } set { email = value; } }
+
+        // kravene i parameteren skal opfyldes for at kunne oprette en patient kan ses nedenunder.
+        public Patient(string Navn, int Cprnr, int Tlfnr, string Sygdom, string Adresse, string Email)
+        {
+            this.navn = Navn;
+            this.cprnr = Cprnr;
+            this.tlfnr = Tlfnr;
+            this.sygdom = Sygdom;
+            this.adresse = Adresse;
+            this.email = Email;
+
+
+        }
+        public Patient()
+        {
+
+        }
+    }
+}
---
+++ FysioterapiMVVM/Viewmodel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FysioterapiMVVM
+{
+    public class Viewmodel
+    {
+        //Instancefield
+        private string patientnavn;
+        private int patientcprnr;
+        private int patienttlfnr;
+        private string patientsygdom;
+        private string patientadresse;
+        private string patientemail;
+
+        // Alt dette her er properties kan ses på kendetegnet (get; set;)
+        //properties
+        // get og set som bære => betyder nøjagtig det samme som return.
+        public string PatientNavn { get => patientnavn; set => patientnavn = value; }
+        public int PatientCprnr { get => patientcprnr; set => patientcprnr = value; }
+        public int PatientTlfnr { get => patienttlfnr; set => patienttlfnr = value; }
+        public string PatientSygdom { get => patientsygdom; set => patientsygdom = value; }
+        public string PatientAdresse { get => patientadresse; set => patientadresse = value; }
+        public string PatientEmail { get => patientemail; set => patientemail = value; }
+        public Patient Patienter { get; set; }
+
+        // Vores Observablecollection tager udgangspunkt i vores patienter
+        public ObservableCollection<Patient1> Patientinfo { get; set; }
+        public Viewmodel()
+        {
+            Patientinfo = new ObservableCollection<Patient1>();
+            // testdata           
+            Patientinfo.Add(new Patient("Søren", 1209101789, 31343638,
+                "ryg gigt", "2860 Soeborg, Fredehuseborgvej 34", "AvForSoeren@yahoo.com"));
+            Patientinfo.Add(new Patient("Jens Peter", 0907381715, 31313131,
+                "mødestabil", "4700 Næstved, Brogade 83", "Jenspeter@blackhat.now"));
+            Patienter = new Patient1();
+        }
+    }
+}
