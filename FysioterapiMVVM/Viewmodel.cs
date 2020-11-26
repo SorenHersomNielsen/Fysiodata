ba3eec48b8b08a8a71f9d31fdffbcd5db59aaa56
SorenHersomNielsen <hersom@ellekrattet.dk>
2020-11-26 10:09:17 +0100

    opret patient
    Jeg har lavet en metorde, så brugen kan oprettet patient i systemet.
    Jeg har også tilføjet RelayCommand filen til systemet.

--- FysioterapiMVVM/Viewmodel.cs
+++ FysioterapiMVVM/Viewmodel.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,6 +19,9 @@ namespace FysioterapiMVVM
         private string patientadresse;
         private string patientemail;
 
+        const string URL = "http://localhost:60928/";
+
+
         // Alt dette her er properties kan ses på kendetegnet (get; set;)
         //properties
         // get og set som bære => betyder nøjagtig det samme som return.
@@ -27,6 +32,10 @@ namespace FysioterapiMVVM
         public string PatientAdresse { get => patientadresse; set => patientadresse = value; }
         public string PatientEmail { get => patientemail; set => patientemail = value; }
         public Patient Patienter { get; set; }
+
+        public RelayCommand Tilføjepatient { get; set; }
+        public RelayCommand SletValgtPatient {get; set;}
+
 
         // Vores Observablecollection tager udgangspunkt i vores patienter
         public ObservableCollection<Patient> Patientinfo { get; set; }
@@ -39,6 +48,56 @@ namespace FysioterapiMVVM
             Patientinfo.Add(new Patient("Jens Peter", 0907381715, 31313131,
                 "mødestabil", "4700 Næstved, Brogade 83", "Jenspeter@blackhat.now"));
             Patienter = new Patient();
+
+            Tilføjepatient = new RelayCommand(Opretpatient); 
+        }
+
+
+        /// <summary>
+        /// Metorde tilføjer en patient, som brugen har skabt til databasen samt til vores observablecollection
+        /// </summary>
+
+        public void Opretpatient()
+        {
+            Patient patient = new Patient(PatientNavn, PatientCprnr, PatientTlfnr, PatientSygdom, PatientAdresse, PatientEmail);
+            Patientinfo.Add(patient);
+
+
+            HttpClientHandler handler = new HttpClientHandler();
+            handler.UseDefaultCredentials = true;
+
+            using (var client = new HttpClient(handler))
+            {
+                //Initialize client
+                client.BaseAddress = new Uri(URL);
+                client.DefaultRequestHeaders.Clear();
+
+                //Request JSON format
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                try
+                {
+
+                    //Get all the flower orders from the database
+                    var PatientResponse = client.PostAsJsonAsync<Patient>("api/Patientstabels", patient).Result;
+
+                    //Check response -> throw exception if NOT successful
+                    PatientResponse.EnsureSuccessStatusCode();
+
+                    //Get the hotels as a ICollection
+                    var flowerOrder = PatientResponse.Content.ReadAsAsync<Patient>().Result;
+
+                    Tilføjepatient.RaiseCanExecuteChanged();
+                }
+                catch
+                {
+
+                }
+
+
+            }
+
+
         }
     }
 }
